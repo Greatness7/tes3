53fb3fe0ae2271d233db69b43942dcffb153beb4
Greatness7 <Greatness7@gmail.com>
2023-03-31 05:15:19 -0400

    Add `serde_json` test.

--- libs/esp/tests/esp_integration.rs
+++ libs/esp/tests/esp_integration.rs
@@ -25,3 +25,22 @@ fn load_save() -> std::io::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn load_save_json() -> std::io::Result<()> {
+    let src_path = "tests/assets/all_types.esp";
+
+    let mut plugin1 = Plugin::new();
+    plugin1.load_path(src_path)?;
+
+    let json_objects = serde_json::to_string(&plugin1.objects).unwrap();
+
+    let mut plugin2 = Plugin::new();
+    plugin2.objects = serde_json::from_str(&json_objects).unwrap();
+
+    let plugin1_bytes = plugin1.save_bytes()?;
+    let plugin2_bytes = plugin2.save_bytes()?;
+    assert_eq!(plugin1_bytes, plugin2_bytes);
+
+    Ok(())
+}

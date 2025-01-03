1a89e5aefb544fefc9c817fcc0c161b953527dcf
Moritz Baron <r.fuzzo@gmail.com>
2025-01-03 11:27:56 +0100

    use a union as value in glob

--- libs/esp/src/types/globalvariable.rs
+++ libs/esp/src/types/globalvariable.rs
@@ -6,8 +6,17 @@ use crate::prelude::*;
 pub struct GlobalVariable {
     pub flags: ObjectFlags,
     pub id: String,
-    pub global_type: GlobalType,
-    pub value: f32,
+    //pub global_type: GlobalType,
+    pub value: GlobalValue,
+}
+
+#[esp_meta]
+#[derive(Clone, Debug, PartialEq, SmartDefault)]
+pub enum GlobalValue {
+    #[default]
+    Float(f32),
+    Short(i16),
+    Long(i32),
 }
 
 impl Load for GlobalVariable {
@@ -16,6 +25,10 @@ impl Load for GlobalVariable {
 
         this.flags = stream.load()?;
 
+        //  MW is very lenient with subrecord orderm, so it might be a case where you load the value before loadign the type
+        let mut temp_val = 0.0;
+        let mut global_type = GlobalType::Short;
+
         while let Ok(tag) = stream.load() {
             match &tag {
                 b"NAME" => {
@@ -23,13 +36,13 @@ impl Load for GlobalVariable {
                 }
                 b"FNAM" => {
                     stream.expect(1u32)?;
-                    this.global_type = stream.load()?;
+                    global_type = stream.load()?;
                 }
                 b"FLTV" => {
                     stream.expect(4u32)?;
-                    let value: f32 = stream.load()?;
+                    temp_val = stream.load::<f32>()?;
                     // Ignore NaNs, see "ratskilled" in "Morrowind.esm".
-                    this.value = if value.is_nan() { 0.0 } else { value };
+                    temp_val = if temp_val.is_nan() { 0.0 } else { temp_val };
                 }
                 b"DELE" => {
                     let size: u32 = stream.load()?;
@@ -40,6 +53,13 @@ impl Load for GlobalVariable {
                     Reader::error(format!("Unexpected Tag: {}::{}", this.tag_str(), tag.to_str_lossy()))?;
                 }
             }
+        }
+
+        // now save the temp value
+        match global_type {
+            GlobalType::Short => this.value = GlobalValue::Short(temp_val as i16),
+            GlobalType::Long => this.value = GlobalValue::Long(temp_val as i32),
+            GlobalType::Float => this.value = GlobalValue::Float(temp_val),
         }
 
         Ok(this)
@@ -55,11 +75,22 @@ impl Save for GlobalVariable {
         // FNAM
         stream.save(b"FNAM")?;
         stream.save(&1u32)?;
-        stream.save(&self.global_type)?;
+        let global_type = match self.value {
+            GlobalValue::Float(_) => GlobalType::Float,
+            GlobalValue::Short(_) => GlobalType::Short,
+            GlobalValue::Long(_) => GlobalType::Long,
+        };
+        stream.save(&global_type)?;
         // FLTV
         stream.save(b"FLTV")?;
         stream.save(&4u32)?;
-        stream.save(&self.value)?;
+        // save as f32
+        let value = match self.value {
+            GlobalValue::Float(value) => value,
+            GlobalValue::Short(value) => value as f32,
+            GlobalValue::Long(value) => value as f32,
+        };
+        stream.save(&value)?;
         // DELE
         if self.flags.contains(ObjectFlags::DELETED) {
             stream.save(b"DELE")?;
